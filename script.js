e47f3e56846df4318eb2de8e6d538a1f17006f58
Curtis Laurence Chadwell <>
2021-04-06 15:09:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let array = [3,9,2,5,1]
+
+
+const max = array =>{
+  let currentMax = array[0]
+  for (let i = 1; i<array.length; i++){
+    if(array[i] > currentMax){
+      currentMax = array[i]
+    }else{
+      continue
+    }
+  }
+
+  return currentMax
+}
+
+console.log(max(array))
+
+
